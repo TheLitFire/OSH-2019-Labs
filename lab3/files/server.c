51eebb64b8f9e9fe713b13d1646489de1ee02e24
TheLitFire <zhao1697801560@gmail.com>
2019-05-22 19:19:43 +0800

    epoll but level-triggered and blocking

--- lab3/files/server.c
+++ lab3/files/server.c
@@ -6,17 +6,23 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/epoll.h>
 #include <errno.h>
-#include <stdbool.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdbool.h>
+#include <sys/sysinfo.h>
+#include <signal.h>
+
 
 #define BIND_IP_ADDR "0.0.0.0"
 #define BIND_PORT 8000
 #define MAX_RECV_LEN 8192
 #define MAX_FILE_LEN 1048576
 #define MAX_SEND_LEN 4096
+#define MAX_EVENT_NUM 1024
 #define handle_error(msg) \
     do { perror(msg); exit(EXIT_FAILURE); } while (0)
 #define HTTP_STATUS_200 "200 OK"
@@ -36,7 +42,7 @@ bool file_read_continue(int fd, char *buf, ssize_t *buf_len, ssize_t buf_reach, 
 int parse_request(int clnt_sock, char* req, struct stat *fstatus){
     ssize_t req_len = read(clnt_sock, req, (size_t)MAX_RECV_LEN); if (req_len == (ssize_t)-1) handle_error("read at parse_request");
     if (req_len < (ssize_t)5 || !file_read_continue(clnt_sock, req, &req_len, (ssize_t)5, (size_t)MAX_RECV_LEN)
-        || req[0] != 'G' || req[1] != 'E' || req[2] != 'T' || req[3] != ' ' || req[4] != '/') return -2;//500
+        || req[0] != 'G' || req[1] != 'E' || req[2] != 'T' || req[3] != ' ' || req[4] != '/') return -2;
     ssize_t s1 = (ssize_t)3, s2 = (ssize_t)5;
     int k = 0;
     while (s2 < req_len || (s2 >= req_len && file_read_continue(clnt_sock, req, &req_len, s2 + (ssize_t)1, (size_t)MAX_RECV_LEN))){
@@ -89,42 +95,72 @@ void handle_clnt(int clnt_sock){
     free(response);
 }
 
-int main(){
-    // 创建套接字，参数说明：
-    //   AF_INET: 使用 IPv4
-    //   SOCK_STREAM: 面向连接的数据传输方式
-    //   IPPROTO_TCP: 使用 TCP 协议
-    int serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); if (serv_sock==-1) handle_error("socket at main");
-    // 将套接字和指定的 IP、端口绑定
-    //   用 0 填充 serv_addr （它是一个 sockaddr_in 结构体）
-    struct sockaddr_in serv_addr;
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    //   设置 IPv4
-    //   设置 IP 地址
-    //   设置端口
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(BIND_IP_ADDR);
-    serv_addr.sin_port = htons(BIND_PORT);
-    //   绑定
-    if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1) handle_error("bind at main");
+void parent_process_wait(int signal){
+    int status;
+    while (waitpid(-1, &status, WNOHANG) > 0);
+}
 
-    // 使得 serv_sock 套接字进入监听状态，开始等待客户端发起请求
+void handle_epoll(int serv_sock){
+    struct epoll_event ep_ev, *events = (struct epoll_event *)malloc(MAX_EVENT_NUM * sizeof(struct epoll_event));
+    if (events == NULL){
+        fprintf(stderr, "malloc FAIL at handle_epoll");
+        exit(EXIT_FAILURE);
+    } 
+    int ep_fd = epoll_create1(0); if (ep_fd == -1) handle_error("epoll_create");
+    ep_ev.data.fd = serv_sock;
+    ep_ev.events = EPOLLIN;
+    if (-1 == epoll_ctl(ep_fd, EPOLL_CTL_ADD, serv_sock, &ep_ev)) handle_error("epoll_ctl at serv_sock");
+    
+    struct sockaddr_in clnt_addr; socklen_t clnt_size = sizeof(clnt_addr);
+    int nfd, clnt_sock;
+    for (;;){
+        if ((nfd = epoll_wait(ep_fd, events, MAX_EVENT_NUM, -1)) == -1) {
+            perror("epoll_wait"); continue;
+        }
+        for (int i = 0; i < nfd; ++i){
+            if (events[i].data.fd == serv_sock){
+                clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_size);
+                if (clnt_sock == -1){
+                    //perror("accept");
+                    continue;
+                }else{
+                    //if (fcntl(clnt_sock, F_SETFL, fcntl(clnt_sock, F_GETFD) | O_NONBLOCK) == -1) continue;
+                    ep_ev.events = EPOLLIN | EPOLLONESHOT;
+                    ep_ev.data.fd = clnt_sock;
+                    if (epoll_ctl(ep_fd, EPOLL_CTL_ADD, clnt_sock, &ep_ev) == -1){
+                        perror("epoll_ctl at clnt_sock");
+                        close(clnt_sock);
+                    }
+                }
+            }else if (events[i].events & EPOLLIN){
+                handle_clnt(events[i].data.fd);
+            }
+        }
+    }
+}
+
+int main(){
+    int serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); if (serv_sock==-1) handle_error("socket at main");
+    if (fcntl(serv_sock, F_SETFL, O_NONBLOCK) == -1) handle_error("fcntl at main");
+    int flag_on = 1;
+    if (-1 == setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, &flag_on, sizeof(flag_on))) handle_error("setsocketopt at main");
+    
+    struct sockaddr_in serv_addr; memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;//IPv4
+    serv_addr.sin_addr.s_addr = inet_addr(BIND_IP_ADDR);//Address
+    serv_addr.sin_port = htons(BIND_PORT);//Port
+    
+    if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1) handle_error("bind at main");
     if (listen(serv_sock, SOMAXCONN)==-1) handle_error("listen at main");
     
-    // 接收客户端请求，获得一个可以与客户端通信的新的生成的套接字 clnt_sock
-    struct sockaddr_in clnt_addr;
-    socklen_t clnt_addr_size = sizeof(clnt_addr);
-
-    while (1){
-        // 当没有客户端连接时， accept() 会阻塞程序执行，直到有客户端连接进来
-        int clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-        if (clnt_sock==-1) handle_error("accept at main");
-        // 处理客户端的请求
-        handle_clnt(clnt_sock);
+    signal(SIGCHLD, parent_process_wait);
+    int child_process_num = get_nprocs();
+    for (int i = 0; i < child_process_num; ++i){
+        if (fork() == 0){
+            handle_epoll(serv_sock);
+            exit(0);
+        }
     }
-
-    // 实际上这里的代码不可到达，可以在 while 循环中收到 SIGINT 信号时主动 break
-    // 关闭套接字
-    close(serv_sock);
+    while (1) sleep(255);
     return 0;
 }
